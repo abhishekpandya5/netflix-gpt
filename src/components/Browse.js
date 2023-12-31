c14f35d1c15b868f2437090585fee010ec79a34c
Abhishek Pandya <abhishekpandya5@gmail.com>
2024-01-01 01:01:55 +0530

    add movies to redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import { options } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getNowPlayingMovies = async () => {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        options
+      );
+      const json = await data.json();
+      dispatch(addNowPlayingMovies(json.results));
+    };
+    getNowPlayingMovies();
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://avatars.githubusercontent.com/u/30802411?v=4";
+
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTE3NDliNDNlZDk4ZGZjY2ZhMTFlMDcxNjU4MGRjYiIsInN1YiI6IjY1ODg0ZjlhMDcyMTY2NjZkNGE2MjQ4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DMX446JrFLeX1p_A2OCsm5H7IeW5OekuuHC_DQ5XG0U",
+  },
+};
