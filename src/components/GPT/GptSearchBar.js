4d51e78978a9e05f918024a85efc42cf9f54e466
Abhishek Pandya <abhishekpandya5@gmail.com>
2024-03-25 22:02:08 +0530

    openai setup

--- src/components/GPT/GptSearchBar.js
+++ src/components/GPT/GptSearchBar.js
@@ -1,18 +1,38 @@
-import React from "react";
+import { useRef } from "react";
 import { useSelector } from "react-redux";
 import lang from "../../utils/languageConstants";
+import openai from "../../utils/openai";
 
 const GptSearchBar = () => {
   const activeLanguage = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    // console.log(searchText.current.value);
+    const content = searchText.current.value;
+    const result = await openai.chat.completions.create({
+      messages: [{ role: "user", content: content }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(result);
+  };
+
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           placeholder={lang[activeLanguage].gptSearchPlaceholder}
           className="py-2 px-4 m-4 col-span-9"
         />
-        <button className="col-span-3 py-2 px-4 m-4 bg-red-color rounded-lg text-white">
+        <button
+          className="col-span-3 py-2 px-4 m-4 bg-red-color rounded-lg text-white"
+          onClick={handleGptSearchClick}
+        >
           {lang[activeLanguage].search}
         </button>
       </form>
